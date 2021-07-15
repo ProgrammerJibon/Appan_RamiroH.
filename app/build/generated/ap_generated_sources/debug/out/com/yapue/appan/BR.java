e132b96636a1705036aa955b78de1369d5ad2801
MD Jibon Howlader <73585915+ProgrammerJibon@users.noreply.github.com>
2021-07-16 03:54:29 +0600

    Delete app/build/generated directory

--- app/build/generated/ap_generated_sources/debug/out/com/yapue/appan/BR.java
+++
@@ -1,5 +0,0 @@
-package com.yapue.appan;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/trigger/debug/com/yapue/appan/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.yapue.appan;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
